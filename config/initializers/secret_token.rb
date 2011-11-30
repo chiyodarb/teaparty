c7bfdd376e0e2ac9af6ca340188007c624af868f
瀧内 元気 <genki@s21g.com>
2011-11-30 21:37:43 +0900

    Init rails-3.1.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teaparty::Application.config.secret_token = '7f678be6a81e2b48111cc0261dcdf2ff7a3e7dc9c714e14e7b295c206be2d7975546bdb3eeb271e6d35cf5db244f28c26f1302fa7a429f8a1832a6595e6d8698'
