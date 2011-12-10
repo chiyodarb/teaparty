152ca48a3012f1df1e19299c7bdc96bfe702dc3f
瀧内 元気 <genki@s21g.com>
2011-12-11 05:11:40 +0900

    Debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,7 @@ Teaparty::Application.configure do
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
+  config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
